bbe927d6ae435586571061c8cdd867dd6e3f6aaa
Khalid Graine <khalidgraine1@gmail.com>
2023-10-29 18:43:51 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<script setup>
+
+</script>
+
+<template>
+
+  <div class="text-white">
+
+
+    <div class="w-10/12 md:w-6/12 mx-auto my-20 grid   rounded-sm overflow-hidden shadow-sm grid-cols-4">
+
+      <div class="box bg-white flex flex-col gap-4 col-span-4">
+        <h3 class="text-primary-cyan text-lg  font-bold">Join our community</h3>
+        <p class="text-primary-yellow">30-day hasle free money back gurantee</p>
+        <p class="t text-primary-blue">Gain access to our full library of tutorials along with expert code reviews. 
+        Perfect for any developers who are serious about honing their skills.</p>
+      </div>
+       
+
+      <!-- box 2 -->
+      <div class="box flex flex-col gap-4 bg-primary-cyan col-span-4 md:col-span-2">
+        <p class="font-semibold text-xl">Monthly sepscription</p>
+        <div>
+          <div class="flex items-center gap-2">
+            <p class=" text-3xl font-semibold">$29</p>
+            <span class="f text-pri text-primary-gray">per month</span>
+          </div>
+         <p>for acces lorem ththt ehehe </p>
+        </div>
+        <button class="bg-primary-yellow py-2 rounded-md text-white font-bold">Sign up</button>
+      </div>
+
+
+
+      <div class="box flex flex-col gap-4 bg-primary-cyann  col-span-4 md:col-span-2">
+       <p class="font-semibold text-xl">Why us</p>
+       <ul>
+        <li>Tutorials by industry experts</li>
+        <li>Peer &amp; expert code review</li>
+        <li>Coding exercises</li>
+        <li>Access to our GitHub repos</li>
+        <li>Community forum</li>
+        <li>Flashcard decks</li>
+        <li>New videos every week</li>
+       </ul>
+      </div>
+    </div>
+
+
+  </div>
+ 
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        primary:  ['Karla', 'system-ui'],
+      },
+      colors: {
+        primary: {
+          cyan: 'hsl(179, 62%, 43%)',
+          cyann: 'hsl(179, 62%, 70%)',
+          yellow: 'hsl(71, 73%, 54%)',
+          gray: ' hsl(204, 43%, 93%)',
+          blue: 'hsl(218, 22%, 67%)'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+
